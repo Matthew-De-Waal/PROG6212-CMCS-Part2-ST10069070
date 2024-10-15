d7a713fa24a04a5f1a0c14eb6013ea74bb4cc5a2
Matthew De Waal <ST10069070@vcconnect.edu.za>
2024-10-15 19:53:12 +0200

    Project Update. Version: 1.4

--- CMCS/Models/CMCSDocument.cs
+++ CMCS/Models/CMCSDocument.cs
@@ -4,10 +4,10 @@
     {
         // Automatic Properties
         public int DocumentID { get; set; }
-        public string Name { get; set; }
-        public string Type { get; set; }
+        public string? Name { get; set; }
+        public string? Type { get; set; }
         public long Size { get; set; }
-        public string Section { get; set; }
-        public string Content { get; set; }
+        public string? Section { get; set; }
+        public string? Content { get; set; }
     }
 }
